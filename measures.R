868266bb28d24d1aa95a1ea29dcd5eee5a6c60ce
unknown <sam@sr-research.com>
2022-06-17 23:07:23 +0100

    Added a function to measure angle from horizontal plane of saccades (will be needed for plotting data)

--- measures.R
+++ measures.R
@@ -114,3 +114,27 @@ measureArea <- function(index) {
     sintegral(1:num_rows, odists, num_rows)
   }
 }
+
+measureSaccAngle <- function(index) {
+  # return the angle of the main saccade (line from first to last sample)
+  # as measured from the horizontal plane
+  sample_df <<- getSamplesInSaccade(index)
+  if (sum(is.na(sample_df$gxR)) > 0) {
+    print(paste("Index", index, ": Blink detected, ignoring..."), sep=" ")
+  } else {
+    first_sample <- sample_df[1,] # store the starting sample
+    last_sample <- tail(sample_df,1) # store the last sample
+    xdiff <- last_sample$gxR-first_sample$gxR
+    ydiff <- -(last_sample$gyR -first_sample$gyR) #Invert because  top left pixel coords
+    
+    #sacc_slope <- getSlope(first_sample, last_sample)
+    saccAngle <- atan(ydiff/xdiff)*180/pi
+    if(xdiff<0 && ydiff >0){
+      saccAngle <- saccAngle +180
+    }
+    if(xdiff<0 && ydiff <0){
+      saccAngle <- saccAngle -180
+    }
+  }
+  saccAngle
+}
