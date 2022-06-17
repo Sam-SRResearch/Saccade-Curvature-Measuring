d065049901c77fd8ae26a9c214d0aa06b33bfc63
unknown <sam@sr-research.com>
2022-06-17 23:36:48 +0100

    Moved saccade angle function to helpers.R and added mainSaccAngle to measureAll()

--- helpers.R
+++ helpers.R
@@ -58,3 +58,18 @@ getOrthogonalDistance <- function(firstSample,lastSample, currentSample){
   y0 <-currentSample$gyR
   abs((x2-x1)*(y1-y0)-(x1-x0)*(y2-y1))/sqrt((x2-x1)^2+(y2-y1)^2)
 }
+
+getMainSaccAngle <- function(firstSample, lastSample){
+  # Compute the angle of the main saccade trajectory (straight line between first and last sample)
+  # compared to horizontal,using Data Viewer convention of 0-180 and 0- -180
+  xdiff <- last_sample$gxR-first_sample$gxR
+  ydiff <- -(last_sample$gyR -first_sample$gyR) #Invert because  top left pixel coords
+  saccAngle <- atan(ydiff/xdiff)*180/pi
+  if(xdiff<0 && ydiff >0){
+    saccAngle <- saccAngle +180
+  }
+  if(xdiff<0 && ydiff <0){
+    saccAngle <- saccAngle -180
+  }
+ saccAngle 
+}
--- measures.R
+++ measures.R
@@ -11,6 +11,7 @@ measureAll <- function() {
   last_sample_amp <- getAmplitude(last_sample, first_sample)
   
   sacc_slope <- getSlope(first_sample,last_sample)
+  mainSaccAngle <- getMainSaccAngle(first_sample,last_sample)
   
   num_rows <- nrow(sample_df)
   
@@ -32,7 +33,7 @@ measureAll <- function() {
     }
   }
   c(median(angles[!is.na(angles)]), # get median angle (filtering out NA's first)
-    mean(odists[!is.na(odists)]),max(odists)) # get mean and max orthogonal distance in pixels: TO DO: turn to degs of VA
+    mean(odists[!is.na(odists)]),max(odists),mainSaccAngle) # get mean and max orthogonal distance in pixels: TO DO: turn to degs of VA
 }
 
 measureAngle <- function(index) {
@@ -113,28 +114,4 @@ measureArea <- function(index) {
     
     sintegral(1:num_rows, odists, num_rows)
   }
-}
-
-measureSaccAngle <- function(index) {
-  # return the angle of the main saccade (line from first to last sample)
-  # as measured from the horizontal plane
-  sample_df <<- getSamplesInSaccade(index)
-  if (sum(is.na(sample_df$gxR)) > 0) {
-    print(paste("Index", index, ": Blink detected, ignoring..."), sep=" ")
-  } else {
-    first_sample <- sample_df[1,] # store the starting sample
-    last_sample <- tail(sample_df,1) # store the last sample
-    xdiff <- last_sample$gxR-first_sample$gxR
-    ydiff <- -(last_sample$gyR -first_sample$gyR) #Invert because  top left pixel coords
-    
-    #sacc_slope <- getSlope(first_sample, last_sample)
-    saccAngle <- atan(ydiff/xdiff)*180/pi
-    if(xdiff<0 && ydiff >0){
-      saccAngle <- saccAngle +180
-    }
-    if(xdiff<0 && ydiff <0){
-      saccAngle <- saccAngle -180
-    }
-  }
-  saccAngle
 }
