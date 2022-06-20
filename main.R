474ab27f0a704a7bb95e35fe4aff547b96f12cb8
Jeffery Zhan <jefferyzhan84@gmail.com>
2022-06-20 15:20:35 -0400

    fixed typos, and amplitude check

--- main.R
+++ main.R
@@ -3,19 +3,19 @@ library(data.table)
 
 source("./measures.R")
 
-gaze <- read_edf('data/14.edf', import_recordings=FALSE, 
+gaze <- read_edf('data/jim.edf', import_recordings=FALSE, 
                  import_saccades = TRUE, import_blinks=FALSE, 
                  import_fixations=FALSE, import_variables=FALSE, 
                  sample_attributes = c('time', 'gx', 'gy', 'rx', 'ry'))
 
 applyToAll <- function(metric=1) {
   measures <- list('measureAll', 'measureAngle', 'measureOrtho', 'measureArea')
-  
+
   num_row <- nrow(gaze$saccades)
   vec <<- rep(NA, num_row)
-  
+
   message("Calculating curvature metrics... (this may take a minute)")
-  
+
   for (row_index in 1:num_row) {
     sample_df <<- getSamplesInSaccade(row_index)
 
@@ -49,10 +49,10 @@ applyToAll2 <- function(metric=0) {
       saccade_samples <- all_samples[time >= saccade$sttime & time <=saccade$entime] # collect all samples in saccade
       
       if (!anyNA(saccade_samples$gxR)) {
-        median_angles[saccade_index] <- measureAngle2(saccade_samples)
+        median_angle[saccade_index] <- measureAngle2(saccade_samples)
       }
     }
-    return_table %>% cbind(median_angles)
+    return_table %>% cbind(median_angle)
     
   } else if (metric == 2) {
     
@@ -66,11 +66,11 @@ applyToAll2 <- function(metric=0) {
       if (!anyNA(saccade_samples$gxR)) {
         results <- measureOrtho2(saccade_samples)
         # print(results)
-        mean_odists[saccade_index] <- results[1]
-        max_odists[saccade_index] <- results[2]
+        mean_odist[saccade_index] <- results[1]
+        max_odist[saccade_index] <- results[2]
       }
     }
-    return_table %>% cbind(mean_odists, max_odists)
+    return_table %>% cbind(mean_odist, max_odist)
     
   } else if (metric == 3) {
     
@@ -90,50 +90,50 @@ applyToAll2 <- function(metric=0) {
   }
 }
 
-# Test Loop 
-# Fix for jim.edf There seems to be some bug in the EDF reader - all the saccades are in trial 1
-# num_row <-  nrow(gaze$saccades[gaze$saccades$trial==1,]) 
-num_row <- nrow(gaze$saccades)
-resultsTable <-  matrix(NA, nrow=num_row, ncol=5) # Prepare table
-for (row_index in 1:num_row) {
-  sample_df <<- getSamplesInSaccade(row_index)
-  ampcheck <- getAmplitude(sample_df[1,],tail(sample_df,1))
-  if (sum(is.na(sample_df$gxR)) == 0 & ampcheck >= 2) {
-    resultsTable[row_index,] <- measureAll()
-  }
-}
-
-# Attempt radial plot
-library('ggplot2')
-breaks = c(0,15,45,75,105,135,165,195,225,255,285,315,345,360) # Make breaks including half-sized first / last breaks
-fortyfive = c(0,45,90,135,180,225,270,315)                     # For major gridlines
-ninety = c(0,90,180,270)                                       # For red lines
-
-# Code to classify each saccade by angle bin
-df <- as.data.frame(resultsTable)
-df$angles <- as.numeric(df$V4+360)%%360 # Make +/- 180 ((data into 0-360 deg data
-df$anglebin <- as.numeric(cut(df$angles, breaks=breaks, right=TRUE))
-colnames(df) <- c("MedSlope","MeanOrth","MaxOrth","trial","Angle","Angle360","AngleBin")
-df$AngleBin[df$AngleBin==13] <- 1 #Merge the first / last half sized segments
-binmeans <-aggregate(df$MedSlope, list(df$AngleBin), mean, na.rm=TRUE) #change MedSlope for MeanOrth etc
-binmeans <- binmeans[,2]
-
-plotdf <- data.frame(x = seq(0,359,30),y = binmeans)                        # Combine into data frame
-plot<- ggplot(plotdf, aes(x,y)) +
-  geom_col(colour = "black",fill = "light blue", alpha = 0.5) +  # make plot
-  scale_x_continuous(breaks = fortyfive,limits = c(-15, 345)) +  # shift limits by 15 degrees
-  geom_vline(xintercept = ninety, colour = "red") +              # draw red cross hairs
-  coord_polar(start = -105/360*2*pi, direction = -1)           # turn cols to polar and offset by 105 degrees (converted to radians) so that 0 is East
-plot
-
-
-# Code to plot Numbmer of saccades as function of angles
-angle_count <- as.data.frame(table(df$AngleBin))                          #count freq of saccades at different angles
-                                                 
-plotdf <- data.frame(x = seq(0,359,30),y = angle_count$Freq)                        # Combine into data frame
-plot<- ggplot(plotdf, aes(x,y)) +
-  geom_col(colour = "black",fill = "light blue", alpha = 0.5) +  # make plot
-  scale_x_continuous(breaks = fortyfive,limits = c(-15, 345)) +  # shift limits by 15 degrees
-  geom_vline(xintercept = ninety, colour = "red") +              # draw red cross hairs
-  coord_polar(start = -105/360*2*pi, direction = -1)           # turn cols to polar and offset by 105 degrees (converted to radians) so that 0 is East
-plot
+# # Test Loop 
+# # Fix for jim.edf There seems to be some bug in the EDF reader - all the saccades are in trial 1
+# # num_row <-  nrow(gaze$saccades[gaze$saccades$trial==1,]) 
+# num_row <- nrow(gaze$saccades)
+# resultsTable <-  matrix(NA, nrow=num_row, ncol=5) # Prepare table
+# for (row_index in 1:num_row) {
+#   sample_df <<- getSamplesInSaccade(row_index)
+#   ampcheck <- getAmplitude(sample_df[1,],tail(sample_df,1))
+#   if (sum(is.na(sample_df$gxR)) == 0 & ampcheck >= 2) {
+#     resultsTable[row_index,] <- measureAll()
+#   }
+# }
+# 
+# # Attempt radial plot
+# library('ggplot2')
+# breaks = c(0,15,45,75,105,135,165,195,225,255,285,315,345,360) # Make breaks including half-sized first / last breaks
+# fortyfive = c(0,45,90,135,180,225,270,315)                     # For major gridlines
+# ninety = c(0,90,180,270)                                       # For red lines
+# 
+# # Code to classify each saccade by angle bin
+# df <- as.data.frame(resultsTable)
+# df$angles <- as.numeric(df$V4+360)%%360 # Make +/- 180 ((data into 0-360 deg data
+# df$anglebin <- as.numeric(cut(df$angles, breaks=breaks, right=TRUE))
+# colnames(df) <- c("MedSlope","MeanOrth","MaxOrth","trial","Angle","Angle360","AngleBin")
+# df$AngleBin[df$AngleBin==13] <- 1 #Merge the first / last half sized segments
+# binmeans <-aggregate(df$MedSlope, list(df$AngleBin), mean, na.rm=TRUE) #change MedSlope for MeanOrth etc
+# binmeans <- binmeans[,2]
+# 
+# plotdf <- data.frame(x = seq(0,359,30),y = binmeans)                        # Combine into data frame
+# plot<- ggplot(plotdf, aes(x,y)) +
+#   geom_col(colour = "black",fill = "light blue", alpha = 0.5) +  # make plot
+#   scale_x_continuous(breaks = fortyfive,limits = c(-15, 345)) +  # shift limits by 15 degrees
+#   geom_vline(xintercept = ninety, colour = "red") +              # draw red cross hairs
+#   coord_polar(start = -105/360*2*pi, direction = -1)           # turn cols to polar and offset by 105 degrees (converted to radians) so that 0 is East
+# plot
+# 
+# 
+# # Code to plot Numbmer of saccades as function of angles
+# angle_count <- as.data.frame(table(df$AngleBin))                          #count freq of saccades at different angles
+#                                                  
+# plotdf <- data.frame(x = seq(0,359,30),y = angle_count$Freq)                        # Combine into data frame
+# plot<- ggplot(plotdf, aes(x,y)) +
+#   geom_col(colour = "black",fill = "light blue", alpha = 0.5) +  # make plot
+#   scale_x_continuous(breaks = fortyfive,limits = c(-15, 345)) +  # shift limits by 15 degrees
+#   geom_vline(xintercept = ninety, colour = "red") +              # draw red cross hairs
+#   coord_polar(start = -105/360*2*pi, direction = -1)           # turn cols to polar and offset by 105 degrees (converted to radians) so that 0 is East
+# plot
--- measures.R
+++ measures.R
@@ -9,19 +9,19 @@ measureAll <- function() {
   first_sample <<- sample_df[1,] # store the starting sample
   last_sample <<- tail(sample_df,1) # store the last sample
   last_sample_amp <- getAmplitude(last_sample, first_sample)
-  
+
   #TODO: if last_sample_amp < 2.0 degs then skip all the below
-  
+
   sacc_slope <- getSlope(first_sample,last_sample)
   mainSaccAngle <- getMainSaccAngle(first_sample,last_sample) # why does this only work if first/last_sample is <<-?
-  
+
   num_rows <- nrow(sample_df)
-  
+
   angles <- rep(NA, num_rows) # initialize vector to fill in with angle values
   odists <- rep(NA, num_rows) # initialize vector to fill in with odist values
 
   for (row_index in 1:num_rows) {
-    
+
     sample <- sample_df[row_index, ] # get sample for current index
     famplitude <- getAmplitude(sample, first_sample)
     lamplitude <- getAmplitude(sample, last_sample)
@@ -30,7 +30,7 @@ measureAll <- function() {
     if (famplitude >= 0.5 & lamplitude >= 0.5) {
       slope <- getSlope(first_sample,sample)
       angle <- getAngle(sacc_slope, slope)
-      
+
       angles[row_index] <- angle
     }
   }
@@ -46,21 +46,21 @@ measureAngle <- function(index) {
   first_sample <- sample_df[1,] # store the starting sample
   last_sample <- tail(sample_df,1) # store the last sample
   last_sample_amp <- getAmplitude(last_sample, first_sample)
-  
+
   sacc_slope <- getSlope(first_sample, last_sample)
-  
+
   num_rows <- nrow(sample_df)
-  
+
   angles <- rep(NA, num_rows) # initialize vector to fill in with angle values
-  
+
   for (row_index in 1:num_rows) {
     sample <- sample_df[row_index, ] # get sample for current index
     amplitude <- getAmplitude(sample, first_sample)
-    
+
     if (amplitude >= 0.5 || last_sample_amp - amplitude >= 0.5) {
       slope <- getSlope(first_sample,sample)
       angle <- getAngle(sacc_slope, slope)
-      
+
       angles[row_index] <- angle
     }
   }
@@ -75,11 +75,11 @@ measureAngle2 <- function(samples) {
   last_sample <- tail(samples, 1) # get last sample of saccade
   
   first_slope <- getSlope2(last_sample, first_sample) # get base slope
-  last_amplitude <- getAmplitude2(last_sample, first_sample)
+
+  famplitude <- getAmplitude2(samples, first_sample)
+  lamplitude <- getAmplitude2(samples, last_sample)
   
-  amplitudes <- getAmplitude2(samples, first_sample) # get amplitudes of samples in saccade
-  
-  samples <- samples[amplitudes >= 0.5] # filter out samples with amplitudes >= 0.5
+  samples <- samples[famplitudes >= 0.5 & lamplitudes >= 0.5] # filter out samples with amplitudes >= 0.5
   
   slopes <- getSlope2(samples, first_sample)
   angles <- getAngle2(slopes, first_slope)
@@ -88,15 +88,15 @@ measureAngle2 <- function(samples) {
 }
 
 measureOrtho <- function(index) {
-  # return a vector with the mean and max of the orthogonal distances between 
+  # return a vector with the mean and max of the orthogonal distances between
   # each sample and the line between first and last sample
   first_sample <- sample_df[1,] # store the starting sample
   last_sample <- tail(sample_df,1) # store the last sample
 
   num_rows <- nrow(sample_df)
-  
+
   odists <- rep(NA, num_rows) # initialize vector to fill in with odist values
-  
+
   for (row_index in 1:num_rows) {
     sample <- sample_df[row_index, ] # get sample for current index
 
@@ -121,18 +121,18 @@ measureArea <- function(index) {
   # return area under the curve of the saccade
   first_sample <- sample_df[1,] # store the starting sample
   last_sample <- tail(sample_df,1) # store the last sample
-  
+
   num_rows <- nrow(sample_df)
-  
+
   odists <- rep(NA, num_rows)
-  
+
   for (row_index in 1:num_rows) {
     sample <- sample_df[row_index, ] # get sample for current index
-    
+
     odist <- getOrthogonalDistance(first_sample,last_sample,sample)
     odists[row_index] <- odist
   }
-  
+
   sintegral(1:num_rows, odists, num_rows)$int
 }
 
